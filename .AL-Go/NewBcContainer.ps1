11d9506442762d3a9edef93f59fd4b0b056d02a8
Freddy Kristiansen <freddyk@microsoft.com>
2022-03-18 00:00:22 +0100

    versioning -1

--- .AL-Go/NewBcContainer.ps1
+++ .AL-Go/NewBcContainer.ps1
@@ -4,18 +4,20 @@ Param(
 
 $parameters.multitenant = $false
 $parameters.RunSandboxAsOnPrem = $true
-if ("$env:GITHUB_RUN_ID" -eq "") {
+#if ("$env:GITHUB_RUN_ID" -eq "") {
     $parameters.includeAL = $true
     $parameters.doNotExportObjectsToText = $true
     $parameters.shortcuts = "none"
-}
+#}
+
+$parameters.doNotCheckHealth = $true
 
 New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
     Write-Host "Removing $($_.Name)"
-    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
+    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -Force
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
--- .AL-Go/PublishBcContainerApp.ps1
+++ .AL-Go/PublishBcContainerApp.ps1
@@ -5,9 +5,11 @@ Param(
 Publish-BcContainerApp @parameters
 
 $filename = [System.IO.Path]::GetFileName($parameters.appFile)
+$packagesFolder = Join-Path ([System.IO.Path]::GetDirectoryName($parameters.appFile)) "..\.packages"
+if (-not (Test-Path $packagesFolder)) {
+    $packagesFolder = Join-Path ([System.IO.Path]::GetDirectoryName($parameters.appFile)) ".alPackages"
+}
 if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
-    $packagesFolder = Join-Path ([System.IO.Path]::GetDirectoryName($parameters.appFile)) "..\.packages" -Resolve
-
     Write-Host "Publishing Base Application"
     $parameters.appFile = Join-Path $packagesFolder "Microsoft_Base Application.app"
     Publish-BcContainerApp @parameters
@@ -17,8 +19,6 @@ if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
     Publish-BcContainerApp @parameters
 }
 elseif ($filename -like "Microsoft_System Application Test Library_*.*.*.*.app") {
-    $packagesFolder = Join-Path ([System.IO.Path]::GetDirectoryName($parameters.appFile)) "..\.packages" -Resolve
-
     Write-Host "Publishing Tests-TestLibraries"
     $parameters.appFile = Join-Path $packagesFolder "Microsoft_Tests-TestLibraries.app"
     Publish-BcContainerApp @parameters
