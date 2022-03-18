47884df56c24cc7039d3983e51b4650df51d3efb
Freddy Kristiansen <freddyk@microsoft.com>
2022-03-18 07:21:26 +0100

    10G

--- .AL-Go/NewBcContainer.ps1
+++ .AL-Go/NewBcContainer.ps1
@@ -8,16 +8,23 @@ $parameters.includeAL = $true
 $parameters.doNotExportObjectsToText = $true
 $parameters.shortcuts = "none"
 $parameters.doNotCheckHealth = $true
-
-$parameters | Out-Host
-
-$parameters.MemoryLimit = "16G"
+$parameters.MemoryLimit = "10G"
 
 New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
     Write-Host "Removing $($_.Name)"
+    $parameters = @{
+        "containerName" = $parameters.ContainerName
+        "name" = $_.Name
+        "unInstall" = $true
+        "force" = $true
+    }
+    if ($_.Name -ne "Base Application" -and $_.Name -ne "System Application") {
+        $parameters.doNotSaveData = $true
+        $parameters.doNotSaveSchema = $true
+    }
     Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -Force
 }
 
